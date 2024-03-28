7a1d50ddf364ae88d43813cf4a59d628e6f12f67
Raphael Manke <raphael.manke@codecentric.de>
2024-03-28 08:30:52 +0100

    add honeycomb instrumentation

---
+++ src/instrumentation.ts
@@ -0,0 +1,17 @@
+import { getWebAutoInstrumentations } from "@opentelemetry/auto-instrumentations-web";
+import { HoneycombWebSDK } from "@honeycombio/opentelemetry-web";
+
+const sdk = new HoneycombWebSDK({
+  debug: true,
+  //   endpoint: "https://api.honeycomb.io/v1/traces", // US instance
+  endpoint: "https://api.eu1.honeycomb.io/v1/traces", // EU instance
+  apiKey: process.env["HONEYCOMB_API_KEY"], // Replace with your Honeycomb Ingest API Key
+  serviceName: "dealbuilder-customer-frontend", // Replace with your application name. Honeycomb will name your dataset using this variable.
+  instrumentations: [getWebAutoInstrumentations()],
+  //   resourceAttributes: {
+  //     "user.id": user.id, // specific to your app
+  //     "user.role": user.role, // specific to your app
+  //   },
+});
+
+sdk.start();
